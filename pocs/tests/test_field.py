82607cdb079d14170a81f2eddb6922af76f75fae
jamessynge <james.synge@gmail.com>
2018-08-31 14:38:35 -0400

    Test several formats of RA and DEC

--- pocs/tests/test_field.py
+++ pocs/tests/test_field.py
@@ -41,18 +41,34 @@ def test_create_field_Observation_name():
 def test_create_field():
     names = ['oneWord', 'Two Words', '3 Whole - Words']
     right_ascensions = [
-        ('20h00m43.7135s', Longitude('20h00m43.7135s', unit=u.degree)),
+        ('20h00m43.7135s', Longitude('20:00:43.7135', unit=u.hourangle)),
+        ('+20h00m43.7135s', Longitude('20:00:43.7135', unit=u.hourangle)),
+        ('20:00:43.7135', Longitude('20h00m43.7135s', unit=u.degree)),
+        ('+20:00:43.7135', Longitude('20h00m43.7135s', unit=u.degree)),
+        ('12h0m0s', Longitude('180', unit=u.degree)),
+        ('12h0m0s', Longitude('-180', unit=u.degree)),
+        ('12:00:00', Longitude('180', unit=u.degree)),
+        ('12:00:00', Longitude('-180', unit=u.degree)),
         ('0d0m0.0s', Longitude(0, unit=u.degree)),
         ('-0d0m0.0s', Longitude(0, unit=u.degree)),
         ('+0d0m0.0s', Longitude(0, unit=u.degree)),
+        ('0:0:0.0', Longitude(0, unit=u.degree)),
+        ('-0:0:0', Longitude(0, unit=u.degree)),
+        ('+0:0:0', Longitude(0, unit=u.degree)),
     ]
     declinations = [
-        ('22d42m39.0645', Latitude('22d42m39.0645', unit=u.degree)),
-        ('+22d42m39.0645', Latitude('22d42m39.0645', unit=u.degree)),
-        ('-22d42m39.0645', Latitude('-22d42m39.0645', unit=u.degree)),
+        ('22d42m39.0645s', Latitude('22:42:39.0645', unit=u.degree)),
+        ('+22d42m39.0645s', Latitude('22:42:39.0645', unit=u.degree)),
+        ('-22d42m39.0645s', Latitude('-22:42:39.0645', unit=u.degree)),
+        ('22:42:39.0645', Latitude('22d42m39.0645', unit=u.degree)),
+        ('+22:42:39.0645', Latitude('22d42m39.0645', unit=u.degree)),
+        ('-22:42:39.0645', Latitude('-22d42m39.0645', unit=u.degree)),
         ('0d0m0.0s', Latitude(0, unit=u.degree)),
         ('+0d0m0.0s', Latitude(0, unit=u.degree)),
         ('-0d0m0.0s', Latitude(0, unit=u.degree)),
+        ('0:0:0', Latitude(0, unit=u.degree)),
+        ('-0:0:0.0', Latitude(0, unit=u.degree)),
+        ('+0:0:0', Latitude(0, unit=u.degree)),
     ]
 
     for name in names:
@@ -60,5 +76,5 @@ def test_create_field():
             for dec_str, dec in declinations:
                 field = Field(name, '%s %s' % (ra_str, dec_str))
                 assert field.name == name
-                assert field.coord.ra == ra
-                assert field.coord.dec == dec
+                assert u.isclose(field.coord.ra, ra)
+                assert u.isclose(field.coord.dec, dec)
